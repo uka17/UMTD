18f78751bb05d8298ca4b5fb3738a01dac9bd454
Captain lifeguard <unkind@bk.ru>
2016-10-17 22:46:05 +0300

    Unit test project was added

---
+++ UMTD.Tests/ReferenceTestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UMTD.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UMTD.Tests
+{
+    [TestClass]
+    public class ReferenceTestController
+    {
+        private UMTDEntities dbContext = new UMTDEntities();
+
+        [TestMethod]
+        public void LanguageList()
+        {
+            List<prcLanguageList_Result> LanguageList = (from s in dbContext.prcLanguageList()
+                                                         select s).ToList();
+            Assert.AreEqual(1, (from s in LanguageList where s.Name == "Русский" select s.Id).FirstOrDefault());
+            Assert.AreEqual(2, (from s in LanguageList where s.Name == "English" select s.Id).FirstOrDefault());
+        }
+    }
+}
